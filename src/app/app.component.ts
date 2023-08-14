da406bfc01cf9ed63de4f2b206358ac34f71cf61
Kifor Kostiantyn <k.kifor@tenantcloud.com>
2023-08-14 16:39:09 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+	selector: 'app-root',
+	standalone: true,
+	imports: [CommonModule, RouterOutlet],
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {}
